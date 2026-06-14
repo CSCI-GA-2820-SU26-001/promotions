334245ff517e8951db4dbd67998c6af476ebcb61
avatarpeanut <avatarpeanut@gmail.com>
2026-06-14 19:45:59 -0400

    replace YourResourceModel -> Promotion, removed test case example

--- tests/factories.py
+++ tests/factories.py
@@ -3,16 +3,16 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from service.models import Promotion
 
 
-class YourResourceModelFactory(factory.Factory):
+class PromotionFactory(factory.Factory):
     """Creates fake pets that you don't have to feed"""
 
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = YourResourceModel
+        model = Promotion
 
     id = factory.Sequence(lambda n: n)
     name = factory.Faker("first_name")
